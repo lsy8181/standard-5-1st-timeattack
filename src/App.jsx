398df60ace3a1d8d7ea2f175b389090b22fe41ed
lsy8181 <a4630486@gmail.com>
2024-05-20 15:50:30 +0900

    계산기 업로드

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState("");
+
+  const hadleChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const hadleadd = () => {
+    setCount(count + Number(number));
+  };
+
+  const hadleSub = () => {
+    setCount(count - Number(number));
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setNumber("");
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input value={number} onChange={hadleChange} /> 만큼을{" "}
+        <button onClick={hadleadd}>더할게요</button>{" "}
+        <button onClick={hadleSub}>뺄게요</button>
+        <button onClick={handleReset}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{count}</p>
       </div>
     </div>
   );
